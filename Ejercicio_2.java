74cad9ea17b826431e70a714823453d0c344b5d7
Chewiz <57515863+Chewiz@users.noreply.github.com>
2020-04-21 17:45:48 -0500

    Add files via upload

--- Ejercicio_2.java
+++ Ejercicio_2.java
@@ -1,46 +1,29 @@
 
+
+
 /**
  * Realizar un programa que contenga un método que su parámetro sea una cadena de números enteros, 
  * y que realice la suma de todos los números de un solo dígito que se puedan extraer del número dado, 
  * la suma ahora será el número a abstraer dígitos, repetir hasta obtener un número de un solo digito y retornarlo.
  * 
  */
+
+
 public class Ejercicio_2
 {
-    //metodo de prueba
-    public static void main(String args[]){
-        int array[] = {1,5,5};
-        System.out.println(sumaDig(array));
+    public static void main(String args[]){//El metodo main solo funge coomo un metodo de prueba
+        System.out.println(sumaDig(args[0]));//Imprimimos el valor obtenido del metodo sumaDig
     }
     
-    //Metodo que suma los digitos y solo retorna numeros de 1 digito
-    
-    public static int sumaDig(int nums[])
-    {
-        int suma = 0, j=0;
-        boolean band = false;
-        
+    public static int sumaDig(String nums){//El metodo recibe un un string de numeros y retorna un valor entero
+        int sumaD;//Esta variable se utiliza para realizar la suma y es la variable que se retornara
         do{
-            for(int i = 0; i < nums.length-j; i++){
-                suma += nums[i];
+            sumaD = 0;
+            for(int i = 0; i < nums.length(); i++){
+                sumaD += Integer.parseInt(""+nums.charAt(i));//Fuerzo el caracter obtenido del "String nums" a ser un string para hacer la conversión
             }
-            if(suma < 10){
-                band = true;
-            }else{
-                j++;
-                
-                suma =0;
-            }
-        }while(band ==false);
-        return suma;
+            nums =""+sumaD;//intercambiamos los digitos originales, por los de la suma resultante para poder realizar nuevamente las operaciones de ser necesario
+        }while(sumaD > 9);//Aquí termina el metodo do while. El cual determina la condición del problema
+        return sumaD;//Retornamos la suma de los digitos bajo las condiciones dadas por el problema en forma de un valor entero
     }
-    
 }
-
-
-     
-        
-
-
-
-
